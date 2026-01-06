f6fed8a5c08319d0b24408df1969c057c98372bd
arsahak <arsahak99@gmail.com>
2026-01-06 16:58:35 +0600

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import Footer from "@/component/layout/Footer";
+import Navbar from "@/component/layout/Navbar";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -27,7 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
